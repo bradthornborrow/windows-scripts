640cd716a16d63841b70e2fe3f626cf77e7bf290
Brad Thornborrow <brad@thedeskofbrad.ca>
2022-12-24 15:24:33 -0500

    Add new scripts

--- FlipFlopWheel.ps1
+++ FlipFlopWheel.ps1
@@ -1,1 +1,24 @@
-$mode = Read-host "How do you like your mouse scroll (0 or 1)?"; Get-PnpDevice -Class Mouse -PresentOnly -Status OK | ForEach-Object { "$($_.Name): $($_.DeviceID)"; Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters" -Name FlipFlopWheel -Value $mode; "+--- Value of FlipFlopWheel is set to " + (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters").FlipFlopWheel + "`n" }
+#
+# Check to see if we are currently running as an administrator runAs if needed
+#
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+#
+# Change registry settings for all mice present
+#
+# Reverse mouse wheel scroll FlipFlopWheel = 1 
+# Normal mouse wheel scroll FlipFlopWheel = 0 
+#
+$mode = Read-host "How do you like your mouse scroll (0 or 1)?"
+Get-PnpDevice -Class Mouse -PresentOnly -Status OK | ForEach-Object { 
+  "$($_.Name): $($_.DeviceID)"; Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters" -Name FlipFlopWheel -Value $mode -Type DWord; "+--- Value of FlipFlopWheel is set to " + (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters").FlipFlopWheel + "`n"
+}
+#
+# Pause before exiting script
+#
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
