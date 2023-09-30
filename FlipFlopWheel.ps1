dd7449e944ae526670a5b5db6dfd51a9504a6971
Brad Thornborrow <brad@thedeskofbrad.ca>
2023-09-30 19:29:10 -0400

    Update scripts

--- FlipFlopWheel.ps1
+++ FlipFlopWheel.ps1
@@ -13,7 +13,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 # Reverse mouse wheel scroll FlipFlopWheel = 1 
 # Normal mouse wheel scroll FlipFlopWheel = 0 
 #
-$mode = Read-host "How do you like your mouse scroll (0 or 1)?"
+$mode = "1"
 Get-PnpDevice -Class Mouse -PresentOnly -Status OK | ForEach-Object { 
   "$($_.Name): $($_.DeviceID)"; Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters" -Name FlipFlopWheel -Value $mode -Type DWord; "+--- Value of FlipFlopWheel is set to " + (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters").FlipFlopWheel + "`n"
 }
