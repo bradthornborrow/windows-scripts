b3e3bafec55e4c2f15125492f24143ca71458b14
Brad Thornborrow <brad@thedeskofbrad.ca>
2023-11-28 21:06:42 -0500

    Remove firefox script

--- Firefox-Defaults.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem -Directory -Path "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles" -Filter '*.default-release' | % {
-   # copy contents to *.default-release folder
-   if($_.Name -match "\.default-release$"){
-      Copy-Item -Path .\firefox\* -Destination $_.FullName -recurse
-  }
-}
